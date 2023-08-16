9b7cf76e00f2ef857ddea5a3c9d8f9e0103c0bc2
이승준 <72748734+HiLeeS@users.noreply.github.com>
2023-08-17 01:57:38 +0900

    Update ReviewRepository.java

--- app/src/main/java/TubeNova/app/repository/ReviewRepository.java
+++ app/src/main/java/TubeNova/app/repository/ReviewRepository.java
@@ -4,14 +4,22 @@ import TubeNova.app.domain.Category;
 import TubeNova.app.domain.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("select r from Review r where r.category in :categories")
     Page<Review> findByCategories(@Param("categories")List<Category> categories, Pageable pageable);
     Page<Review> findByCategory(Category category, Pageable pageable);
+
+    List<Review> findTop10ByCreatedTimeBetween(LocalDateTime start, LocalDateTime end, Sort likes);
+    Page<Review> findReviewByMemberId(Long memberId, Pageable pageable);
+    List<Review> findTop10ByCategoryAndCreatedTimeBetween(Category category, LocalDateTime start, LocalDateTime end, Sort likes);
+
+    Page<Review> findByTitleContaining(String keyword, Pageable pageable);  //findBy'Column'Containing 을 쓰면 컬럼내에 해당된 것들을 찾아준다.
 }
