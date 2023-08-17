a77f0b61450aff9ee0a646057a62ca7c5b4818f8
이승준 <72748734+HiLeeS@users.noreply.github.com>
2023-08-17 15:57:06 +0900

    Update ReviewCreateRequestDto.java

--- app/src/main/java/TubeNova/app/dto/review/ReviewCreateRequestDto.java
+++ app/src/main/java/TubeNova/app/dto/review/ReviewCreateRequestDto.java
@@ -3,6 +3,7 @@ package TubeNova.app.dto.review;
 import TubeNova.app.domain.Category;
 import TubeNova.app.domain.Member;
 import TubeNova.app.domain.Review;
+import jakarta.persistence.Column;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
@@ -10,23 +11,33 @@ import java.util.Optional;
 
 @Getter
 public class ReviewCreateRequestDto {
-    private String title;
-    private String linkURL;
-    private String contents;
-    private int rating;
-    private Category category;
-    private LocalDateTime createdTime;  //따로 받지 않아도 될거 같음
+    private String videoImageUrl;       //영상 썸네일
+    private String videoTitle;          //영상 제목
+    private String channel;             //채널명
+    private String videoDate;           //유튜브 영상 게시일
+    private String title;               //리뷰 제목
+    private String linkURL;             //영상 링크
+    private String contents;            //리뷰 내용
+    private String writer;              //작성자
+    private int rating;                 //별점
+    private Category category;          //카테고리
+    private LocalDateTime createdTime;  //리뷰 생성 날짜
+    private Long id;                    //memberId
 
     public static Review toReview(ReviewCreateRequestDto dto, Member member){
 
         Review review = new Review().builder()
+                .videoImageUrl(dto.getVideoImageUrl())
+                .videoTitle(dto.getVideoTitle())
+                .channel(dto.getChannel())
+                .videoDate(dto.getVideoDate())
                 .title(dto.getTitle())
                 .linkURL(dto.getLinkURL())
+                .writer(member.getName())
                 .contents(dto.getContents())
                 .rating(dto.getRating())
                 .category(dto.getCategory())
                 .member(member)
-                .writer(member.getName())
                 .build();
         return review;
     }
