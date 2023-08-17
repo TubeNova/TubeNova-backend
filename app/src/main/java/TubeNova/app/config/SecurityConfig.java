d875fbed43b80c6c9a1d19f155ba95e3a59b096e
wonoong <dnd5301@gmail.com>
2023-08-18 01:24:50 +0900

    Cross Origin Config Changed

--- app/src/main/java/TubeNova/app/config/SecurityConfig.java
+++ app/src/main/java/TubeNova/app/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("http://tubenova.site/"));
         config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
