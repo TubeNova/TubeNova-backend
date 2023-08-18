58c5449a16d9d44d274a23bc9d5b07d4f8ad6267
이승준 <72748734+HiLeeS@users.noreply.github.com>
2023-08-18 19:45:29 +0900

    Update LikeRepository.java

--- app/src/main/java/TubeNova/app/repository/LikeRepository.java
+++ app/src/main/java/TubeNova/app/repository/LikeRepository.java
@@ -12,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, Long> {
 
-    Optional<Object> findByReview_IdAndMember_Id(Long reviewId, Long memberId);
+    Optional<LikeEntity> findByReview_IdAndMember_Id(Long reviewId, Long memberId);
     List<LikeEntity> findByMember(Member member);
 }
